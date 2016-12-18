ff6adac05d6cfd939a4c15d5c2363d3f762a321d
Ethan Dagner <napen123@gmail.com>
2016-12-17 17:38:24 -0700

    More documentation

--- NetProperty/Serialization/PropertyAttribute.cs
+++ NetProperty/Serialization/PropertyAttribute.cs
@@ -7,8 +7,16 @@ namespace NetProperty.Serialization
     /// </summary>
     public class PropertyAttribute : Attribute
     {
+        /// <summary>
+        /// The name the property has when serialized and deserialized.
+        /// If <c>null</c>, the field/property name is used.
+        /// </summary>
         public string Name;
 
+        /// <summary>
+        /// Constructor to set <see cref="Name"/>.
+        /// </summary>
+        /// <param name="name">The name to use (<seealso cref="Name"/>).</param>
         public PropertyAttribute(string name)
         {
             if (name != null && (name.Contains("=") || name.Contains("~")))
